c7dd4d84d4eac0765b425cad2a500655e4b36bab
apache-98 <william07apache@gmail.com>
2025-05-28 13:53:35 -0500

    centrado titulo

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -16,7 +16,7 @@ export function Formulario() {
   return (
     <>
       {/* ... -> operador de propagacion y me trae todo lo asociado al metodo */}
-      <h1 className=' justify-center text-white text-2xl font-bold mb-16'>Suscríbete para más contenido anime!</h1>
+      <h1 className=' justify-center text-white text-2xl font-bold mb-16 text-center'>Suscríbete para más contenido anime!</h1>
       <form className='justify-center max-w-md mx-auto' onSubmit={handleSubmit(showData)}>
         <div className='relative z-0 w-full mb-5 group"'>
           <input type='text' placeholder="" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"{...register("usuario")} />
